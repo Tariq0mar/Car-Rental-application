de86974f9f517c83e65dba07073734c5db29d0fb
Tariq Omar <tariqomar2004@gmail.com>
2025-06-16 18:34:21 +0300

    create entities

---
+++ CarRentalapplication/CarRentalApplication.DB/Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalApplication.DB.Models;
+
+public class Booking
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int UserId { get; set; }   
+    public int CarId { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public User User { get; set; }
+    public Car Car { get; set; }
+}
