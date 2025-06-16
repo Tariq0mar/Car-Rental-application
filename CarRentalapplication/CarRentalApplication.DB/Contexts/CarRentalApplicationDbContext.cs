7a6ee20667ece46bae3e524c9819a2631e14a086
Tariq Omar <tariqomar2004@gmail.com>
2025-06-16 18:44:37 +0300

    create db context for entities

---
+++ CarRentalapplication/CarRentalApplication.DB/Contexts/CarRentalApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CarRentalApplication.DB.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalApplication.DB.Contexts;
+
+public class CarRentalApplicationDbContext : DbContext
+{
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+}
