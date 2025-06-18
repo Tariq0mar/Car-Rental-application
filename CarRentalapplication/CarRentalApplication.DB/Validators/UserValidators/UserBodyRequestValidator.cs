aeece90299ef00a9d205095fa350c14075305c3d
Tariq Omar <tariqomar2004@gmail.com>
2025-06-18 18:46:33 +0300

    implement User's Models validators

---
+++ CarRentalapplication/CarRentalApplication.DB/Validators/UserValidators/UserBodyRequestValidator.cs
@@ -0,0 +1,51 @@
+﻿using CarRentalApplication.DB.Models.Users;
+using FluentValidation;
+
+namespace CarRentalApplication.DB.Validators.UserValidators;
+
+public class UserBodyRequestValidator : AbstractValidator<UserBodyRequest>
+{
+    public UserBodyRequestValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required.")
+            .MaximumLength(50).WithMessage("First name must not exceed 50 characters.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required.")
+            .MaximumLength(50).WithMessage("Last name must not exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(10).WithMessage("Password must be at least 10 characters long.")
+            .MaximumLength(50).WithMessage("Password must not exceed 50 characters.");
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Phone number is required.")
+            .MaximumLength(20).WithMessage("Phone number must not exceed 20 characters.");
+
+        RuleFor(x => x.AddressLine1)
+            .NotEmpty().WithMessage("Address Line 1 is required.")
+            .MaximumLength(100).WithMessage("Address Line 1 must not exceed 100 characters.");
+
+        RuleFor(x => x.AddressLine2)
+            .MaximumLength(100).WithMessage("Address Line 2 must not exceed 100 characters.")
+            .When(x => !string.IsNullOrWhiteSpace(x.AddressLine2));
+
+        RuleFor(x => x.City)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(50).WithMessage("City must not exceed 50 characters.");
+
+        RuleFor(x => x.Country)
+            .NotEmpty().WithMessage("Country is required.")
+            .MaximumLength(50).WithMessage("Country must not exceed 50 characters.");
+
+        RuleFor(x => x.DriversLicenseNumber)
+            .NotEmpty().WithMessage("Driver's license number is required.")
+            .MaximumLength(20).WithMessage("Driver's license number must not exceed 20 characters.");
+    }
+}
---
+++ CarRentalapplication/CarRentalApplication.DB/Validators/UserValidators/UserFilteringQueryParametersValidator.cs
@@ -0,0 +1,47 @@
+﻿using CarRentalApplication.DB.Models.Users;
+using FluentValidation;
+
+namespace CarRentalApplication.DB.Validators.UserValidators;
+
+public class UserFilteringQueryParametersValidator : AbstractValidator<UserFilteringQueryParameters>
+{
+    public UserFilteringQueryParametersValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0)
+            .WithMessage("PageNumber must be greater than 0.");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100)
+            .WithMessage("PageSize must be between 1 and 100.");
+
+        RuleFor(x => x.MaxDateOfBirth)
+            .GreaterThanOrEqualTo(x => x.MinDateOfBirth)
+            .When(x => x.MinDateOfBirth.HasValue && x.MaxDateOfBirth.HasValue)
+            .WithMessage("MaxDateOfBirth must be greater than or equal to MinDateOfBirth.");
+
+        RuleFor(x => x.FirstName)
+            .MaximumLength(50)
+            .WithMessage("First name must not exceed 50 characters.");
+
+        RuleFor(x => x.LastName)
+            .MaximumLength(50)
+            .WithMessage("Last name must not exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .MaximumLength(100)
+            .WithMessage("Email must not exceed 100 characters.");
+
+        RuleFor(x => x.PhoneNumber)
+            .MaximumLength(20)
+            .WithMessage("Phone number must not exceed 20 characters.");
+
+        RuleFor(x => x.City)
+            .MaximumLength(50)
+            .WithMessage("City name must not exceed 50 characters.");
+
+        RuleFor(x => x.Country)
+            .MaximumLength(50)
+            .WithMessage("Country name must not exceed 50 characters.");
+    }
+}
