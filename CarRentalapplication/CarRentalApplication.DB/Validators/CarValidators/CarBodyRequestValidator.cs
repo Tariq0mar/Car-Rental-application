d0f90f1c96a5924221590e26dcae9314f616f022
Tariq Omar <tariqomar2004@gmail.com>
2025-06-18 18:36:48 +0300

    implement Car's Models validators

---
+++ CarRentalapplication/CarRentalApplication.DB/Validators/CarValidators/CarBodyRequestValidator.cs
@@ -0,0 +1,20 @@
+﻿using CarRentalApplication.DB.Models.Cars;
+using FluentValidation;
+
+namespace CarRentalApplication.DB.Validators.CarValidators;
+
+public class CarBodyRequestValidator : AbstractValidator<CarBodyRequest>
+{
+    public CarBodyRequestValidator()
+    {
+        RuleFor(x => x.Model)
+            .NotEmpty()
+            .WithMessage("Model is required.")
+            .MaximumLength(100)
+            .WithMessage("Model must not exceed 100 characters.");
+
+        RuleFor(x => x.PricePerDay)
+            .GreaterThan(0)
+            .WithMessage("PricePerDay must be greater than 0.");
+    }
+}
---
+++ CarRentalapplication/CarRentalApplication.DB/Validators/CarValidators/CarFilteringQueryParametersValidator.cs
@@ -0,0 +1,29 @@
+﻿using CarRentalApplication.DB.Models.Cars;
+using FluentValidation;
+
+namespace CarRentalApplication.DB.Validators.CarValidators;
+
+public class CarFilteringQueryParametersValidator : AbstractValidator<CarFilteringQueryParameters>
+{
+    public CarFilteringQueryParametersValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0)
+            .WithMessage("PageNumber must be greater than 0.");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100)
+            .WithMessage("PageSize must be between 1 and 100.");
+
+        RuleFor(x => x.MaxPrice)
+            .GreaterThanOrEqualTo(x => x.MinPrice)
+            .When(x => x.MinPrice.HasValue && x.MaxPrice.HasValue)
+            .WithMessage("MaxPrice must be greater than or equal to MinPrice.");
+
+        RuleFor(x => x.Model)
+            .NotEmpty()
+            .WithMessage("Model is required.")
+            .MaximumLength(100)
+            .WithMessage("Model must not exceed 100 characters.");
+    }
+}
